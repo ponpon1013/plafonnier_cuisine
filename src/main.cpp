bb606c8dae92c2dd3ea18d0049f7196d051ead88
nicolas <nicolas@pcOutil.MARAIS>
2018-11-26 14:19:53 +0100

    coding IR behavior. IR recever receives data and sends it to other PIN

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,33 @@
 #include <Arduino.h>
+#include <IRremoteESP8266.h>
+#include <IRrecv.h>
+#include <IRsend.h>
+
+// PIN Configuration
+#define PIN_RECV 14 //PIN wher IR receiver is connected
+#define PIN_SEND 13 //PIN wher IR receiver is connected
+
+
+IRrecv irrecv(PIN_RECV);//IRRecv object declaration
+decode_results results; // IR message received
+IRsend irsend(PIN_SEND);// IRSend object declaration
 
 void setup() {
-  // put your setup code here, to run once:
+  irrecv.enableIRIn();//start the IR receiver
+  irsend.begin();//start the IR send
 }
 
 void loop() {
   // software algorthm
-  // waiting for IR signal
   // if IR signal the signal treatment
-
+  if (irrecv.decode(&results)){
+    // NEC protocole is waited for
+    if (results.decode_type==NEC){
+      irsend.sendNEC(results.value);
+    }
+    irrecv.resume(); // receive new value
+  }
+  delay(100); // wait before check new message
   // software annex
   // WIFI check
   // MQTT check
